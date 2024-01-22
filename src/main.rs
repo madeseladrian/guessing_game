7d7434a60c34d1397752ab49abcbed231a791d1d
mades eladrian <mades.eladrian@gmail.com>
2024-01-21 21:59:18 -0300

    fix bug with input differents of numbers

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess.");
 
@@ -17,8 +15,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Just numbers are allowed!");
+                continue;
+            },
+        };
+
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
@@ -27,7 +32,7 @@ fn main() {
             Ordering::Equal => {
                 println!("You win!");
                 break;
-            }
+            },
         }   
     }
 }
