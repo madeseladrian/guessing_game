eefa2d585fe8142b91e64263f0fa2bac7079efe8
mades eladrian <mades.eladrian@gmail.com>
2024-01-23 09:29:13 -0300

    add types in some variables

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // Generate a random number between 1 and 100
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess: String = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        // Transform a String Number to a Number
         // let guess: u32 = guess.trim().parse().expect("Please type a number!");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
